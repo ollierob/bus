6d20d2e387cd446115217bd0f84d35f6f02b40b0
Ollie Robertshaw <oliver.robertshaw@mahifx.com>
2020-09-07 19:19:18 +0100

    Modules

---
+++ deploy/build/src/main/java/net/ollie/bus/deploy/source/build/BuildSource.java
@@ -0,0 +1,7 @@
+package net.ollie.bus.deploy.source.build;
+
+import net.ollie.bus.deploy.source.DeploymentSource;
+
+public interface BuildSource extends DeploymentSource {
+
+}
--- deploy/core/src/main/java/net/ollie/bus/deploy/source/git/GitSource.java
+++
@@ -1,7 +0,0 @@
-package net.ollie.bus.deploy.source.git;
-
-import net.ollie.bus.deploy.source.DeploymentSource;
-
-public interface GitSource extends DeploymentSource {
-
-}
