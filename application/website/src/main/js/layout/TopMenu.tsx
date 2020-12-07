8638d706f615f7ae171f9ba5f785d21b226fcf0a
Ollie Robertshaw <oliver.robertshaw@mahifx.com>
2020-12-07 20:05:58 +0000

    NPM update

--- application/website/src/main/js/layout/TopMenu.tsx
+++ application/website/src/main/js/layout/TopMenu.tsx
@@ -1,6 +1,6 @@
 import * as React from "react";
 import {Menu} from "antd";
-import {SelectOutlined, VerticalAlignBottomOutlined} from "@ant-design/icons";
+import {CloudDownloadOutlined, SelectOutlined, VerticalAlignBottomOutlined} from "@ant-design/icons";
 
 type Props = {
     open: "deploy" | "manage"
@@ -10,11 +10,11 @@ export default class TopMenu extends React.PureComponent<Props> {
 
     render() {
         return <Menu className="topMenu" mode="horizontal" selectedKeys={[this.props.open]}>
+            <Menu.Item key="provision" icon={<CloudDownloadOutlined/>}>
+                Provision
+            </Menu.Item>
             <Menu.Item key="deploy" icon={<VerticalAlignBottomOutlined/>}>
                 <a href="/deploy/">Deploy</a>
-            </Menu.Item>
-            <Menu.Item key="provision">
-                Provision
             </Menu.Item>
             <Menu.Item key="manage" icon={<SelectOutlined/>}>
                 <a href="/manage/">Manage</a>
