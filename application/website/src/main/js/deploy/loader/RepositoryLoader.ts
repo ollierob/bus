1ebe4d0312b73529de53cc4c70052aa1accd0f0c
Ollie Robertshaw <oliver.robertshaw@mahifx.com>
2020-12-11 18:52:03 +0000

    Repos

--- application/website/src/main/js/deploy/loader/RepositoryLoader.ts
+++ application/website/src/main/js/deploy/loader/RepositoryLoader.ts
@@ -1,7 +1,19 @@
-import {AsyncData} from "../../fetch/AsyncData";
+import {Repository, RepositoryList} from "../../../protobuf/repository_pb";
+import {protoGet} from "../../fetch/ProtoFetch";
 
 export interface RepositoryLoader {
 
-    loadAll(): AsyncData<ReadonlyArray<any>>
+    loadAll(): Promise<ReadonlyArray<Repository.AsObject>>
 
 }
+
+class ProtoRepositoryLoader implements RepositoryLoader {
+
+    loadAll() {
+        return protoGet("/deploy/repository/get", RepositoryList.deserializeBinary)
+            .then(l => l ? l.getRepositoryList().map(r => r.toObject()) : []);
+    }
+
+}
+
+export const DEFAULT_REPO_LOADER: RepositoryLoader = new ProtoRepositoryLoader();
---
+++ application/website/src/main/protobuf/repository_pb.d.ts
@@ -0,0 +1,56 @@
+// package: maven
+// file: repository.proto
+
+import * as jspb from "google-protobuf";
+import * as maven_pb from "./maven_pb";
+
+export class Repository extends jspb.Message {
+  hasMaven(): boolean;
+  clearMaven(): void;
+  getMaven(): maven_pb.MavenRepository | undefined;
+  setMaven(value?: maven_pb.MavenRepository): void;
+
+  getRepoCase(): Repository.RepoCase;
+  serializeBinary(): Uint8Array;
+  toObject(includeInstance?: boolean): Repository.AsObject;
+  static toObject(includeInstance: boolean, msg: Repository): Repository.AsObject;
+  static extensions: {[key: number]: jspb.ExtensionFieldInfo<jspb.Message>};
+  static extensionsBinary: {[key: number]: jspb.ExtensionFieldBinaryInfo<jspb.Message>};
+  static serializeBinaryToWriter(message: Repository, writer: jspb.BinaryWriter): void;
+  static deserializeBinary(bytes: Uint8Array): Repository;
+  static deserializeBinaryFromReader(message: Repository, reader: jspb.BinaryReader): Repository;
+}
+
+export namespace Repository {
+  export type AsObject = {
+    maven?: maven_pb.MavenRepository.AsObject,
+  }
+
+  export enum RepoCase {
+    REPO_NOT_SET = 0,
+    MAVEN = 1,
+  }
+}
+
+export class RepositoryList extends jspb.Message {
+  clearRepositoryList(): void;
+  getRepositoryList(): Array<Repository>;
+  setRepositoryList(value: Array<Repository>): void;
+  addRepository(value?: Repository, index?: number): Repository;
+
+  serializeBinary(): Uint8Array;
+  toObject(includeInstance?: boolean): RepositoryList.AsObject;
+  static toObject(includeInstance: boolean, msg: RepositoryList): RepositoryList.AsObject;
+  static extensions: {[key: number]: jspb.ExtensionFieldInfo<jspb.Message>};
+  static extensionsBinary: {[key: number]: jspb.ExtensionFieldBinaryInfo<jspb.Message>};
+  static serializeBinaryToWriter(message: RepositoryList, writer: jspb.BinaryWriter): void;
+  static deserializeBinary(bytes: Uint8Array): RepositoryList;
+  static deserializeBinaryFromReader(message: RepositoryList, reader: jspb.BinaryReader): RepositoryList;
+}
+
+export namespace RepositoryList {
+  export type AsObject = {
+    repositoryList: Array<Repository.AsObject>,
+  }
+}
+
