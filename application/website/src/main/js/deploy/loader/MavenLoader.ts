7cbd04b13f8e4faba49b73d875913f6a8601fc85
Ollie Robertshaw <oliver.robertshaw@mahifx.com>
2020-12-13 14:34:00 +0000

    Save repo

--- application/website/src/main/js/deploy/loader/MavenLoader.ts
+++ application/website/src/main/js/deploy/loader/MavenLoader.ts
@@ -1,5 +1,5 @@
-import {MavenRepository, MavenRepositoryList} from "../../../protobuf/maven_pb";
-import {protoGet} from "../../fetch/ProtoFetch";
+import {MavenCentral, MavenRepository, MavenRepositoryList, Nexus3Repository} from "../../../protobuf/maven_pb";
+import {protoGet, protoPost} from "../../fetch/ProtoFetch";
 
 export interface MavenLoader {
 
@@ -19,7 +19,8 @@ class ProtoMavenLoader implements MavenLoader {
     }
 
     save(spec: MavenRepository.AsObject) {
-        return undefined;
+        return protoPost("/deploy/repository/maven/save", toProto(spec), MavenRepository.serializeBinaryToWriter, MavenRepository.deserializeBinary)
+            .then(r => r ? r.toObject() : null);
     }
 
     delete(id: string) {
@@ -29,3 +30,21 @@ class ProtoMavenLoader implements MavenLoader {
 }
 
 export const DEFAULT_MAVEN_LOADER: MavenLoader = new ProtoMavenLoader();
+
+function toProto(spec: MavenRepository.AsObject): MavenRepository {
+    const proto = new MavenRepository();
+    if (spec.id) proto.setId(spec.id);
+    if (spec.version) proto.setVersion(spec.version);
+    if (spec.nexus3) {
+        const nexus = new Nexus3Repository();
+        nexus.setUsername(spec.nexus3.username);
+        nexus.setPassword(spec.nexus3.password);
+        nexus.setUrl(spec.nexus3.url);
+        proto.setNexus3(nexus);
+    } else if (spec.central) {
+        const central = new MavenCentral();
+        central.setUrl(spec.central.url);
+        proto.setCentral(central);
+    }
+    return proto;
+}
